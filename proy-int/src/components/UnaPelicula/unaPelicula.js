a62ac547d05b9bd99be2d3c2cd1b1558994bdd15
brisawaj <bwajsbrot@udesa.edu.ar>
2024-09-16 18:27:13 -0300

    agregar lista de peliculas, y mejoras

--- proy-int/src/components/UnaPelicula/unaPelicula.js
+++ proy-int/src/components/UnaPelicula/unaPelicula.js
@@ -1,19 +1,53 @@
 import "./UnaPelicula.css"
-import react, {Component} from 'react';
+import react, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+const IMAGES_URL = 'https://image.tmdb.org/t/p/w1280'
 
 class UnaPelicula extends Component {
-    componentDidMount(){
-        console.log('acaaaaa', this.props.data)
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false,
+        }
+        console.log(this.props);
     }
-    render(){
-        return(
-            <div> 
-                <h2>{this.props.data.name || this.props.data.title} </h2>
-                <p>{this.props.data.popularity}</p>
-            
+
+    componentDidMount() {
+        console.log('acaaaaa', this.props.pelicula)
+    }
+
+    showDescription() {
+        this.setState({ show: true })
+    }
+
+    hideDescription() {
+        this.setState({ show: false })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="titulo">{this.props.pelicula.name || this.props.pelicula.title}</h2>
+                <img className="poster" src={IMAGES_URL + this.props.pelicula.poster_path} />
+                {this.state.show &&
+                    <p>{this.props.pelicula.overview}</p>
+                }
+                <button onClick={() => {
+                    if (this.state.show) {
+                        this.hideDescription()
+                    } else {
+                        this.showDescription()
+                    }
+                }}>{this.state.show ? 'Ocultar' : 'Mostrar'}</button>
+                <button>Agregar a Favoritos</button>
+                <Link to={'/pelicula/533535'}>
+                    Detalle
+                </Link>
             </div>
         )
     }
 }
-  
-  export default UnaPelicula;
+
+export default UnaPelicula;
--- proy-int/src/screens/home/home.js
+++ proy-int/src/screens/home/home.js
@@ -1,58 +1,44 @@
+import { Component } from 'react';
+import ListaPeliculas from "../../components/ListaPeliculas/ListaPeliculas";
 import "./Home.css"
-import UnaPelicula from "../../components/UnaPelicula/UnaPelicula";
-import react, { Component } from 'react';
-import Loader from "../../components/Loader/Loader";
+
 class Home extends Component {
     constructor() {
-
         super()
         this.state = { popular: [], topRated: [], loaderTop: true, loaderPopular: true }
 
     }
 
-    componentDidMount() {
-        fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=8942348488014765a582e61cb7357525')
+    gethMovies(type) {
+        const sortBy = type === 'popular' ? "popularity.desc" : "vote_average.desc"
+        fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=${sortBy}&api_key=8942348488014765a582e61cb7357525`)
             .then(res => res.json())
             .then(data => {
-                console.log(data)
-                this.setState({
-                    popular: data.results.slice(0, 5),
-                    loaderPopular: false
-                })
+                if (type == 'popular') {
+                    this.setState({
+                        popular: data.results.slice(0,5),
+                        loaderPopular: false
+                    })
+                } else if (type == 'top') {
+                    this.setState({
+                        topRated: data.results.slice(0,5),
+                        loaderTopRated: false
+                    })
+                }
             })
             .catch(e => console.log(e))
+    }
 
-        fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200&api_key=8942348488014765a582e61cb7357525')
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                this.setState({
-                    topRated: data.results.slice(0, 5),
-                    loaderTop: false
-                })
-            })
-            .catch(e => console.log(e))
+    componentDidMount() {
+       this.gethMovies('popular')
+       this.gethMovies('top')
     }
 
     render() {
         return (
             <div>
-                <div className="popular-container">
-                <h1>Popular Movies</h1>
-                    {
-                        this.state.loaderPopular ?
-                            <Loader /> :
-                            this.state.popular.map((Peli, idx) => <UnaPelicula data={Peli} key={idx}></UnaPelicula>)
-                    }
-                </div>
-                <div className='topRated-container'> 
-                    <h1>Top Rated Movies</h1>
-                    {
-                        this.state.loaderTopRated ? 
-                            <Loader /> : 
-                            this.state.topRated.map((Peli, idx) => <UnaPelicula data={Peli} key={idx}></UnaPelicula>)
-                    }
-                </div>
+                <ListaPeliculas title={"Popular Movies"} loader={this.state.loaderPopular} peliculas={this.state.popular}/> 
+                <ListaPeliculas title={"Top Rated Movies"} loader={this.state.loaderTopRated} peliculas={this.state.topRated}/> 
             </div>
         )
     }
