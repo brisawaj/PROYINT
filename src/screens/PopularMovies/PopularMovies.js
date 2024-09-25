855ab0371af5cac5d5c9387a3cd96725f3df30c8
Sol Orsi <sorsifernandez@udesa.com.ar>
2024-09-25 00:17:15 -0300

    filtro terminado  de screen populares y top

--- src/screens/PopularMovies/PopularMovies.js
+++ src/screens/PopularMovies/PopularMovies.js
@@ -7,23 +7,33 @@ import Button from '../../components/Button/Button';
 class PopularMovies extends Component {
     constructor() {
         super()
-        this.state = { popular: [], loaderPopular: true }
+        this.state = { popular: [], filtroPopular: [], filtroValue:'', loaderPopular: true }
     }
 
     componentDidMount() {
        getMovies('popularity.desc').then(movies => {
            this.setState({
                 popular: movies,
+                filtroPopular: movies,
                 loaderPopular: false,
             })
        })
     }
 
+    handleFilterChange(e){
+      const userValue= e.target.value
+      this.setState({
+        filtroValue: userValue,
+        filtroPopular: this.state.popular.filter(movie => movie.title.toLowerCase().includes(userValue.toLowerCase()))
+      })
+    }
+
     render() {
         return (
             <div className="home-container">
+                <input type='text' className="input-buscador" placeholder="Filtra película..." onChange= {(e)=>this.handleFilterChange(e)}value={this.state.filtroValue}/>
                 <div className="movies-section">
-                    <ListaPeliculas title={"Popular Movies"} loader={this.state.loaderPopular} peliculas={this.state.popular}/> 
+                    <ListaPeliculas title={"Popular Movies"} loader={this.state.loaderPopular} peliculas={this.state.filtroPopular}/> 
                 </div>
                 <div className='load-more-container'>
                     <Button className="dark">
--- src/screens/Top/Top.js
+++ src/screens/Top/Top.js
@@ -7,23 +7,33 @@ import Button from '../../components/Button/Button';
 class Top extends Component {
     constructor() {
         super()
-        this.state = { popular: [], loaderPopular: true }
+        this.state = { top: [], filtroTop: [], filtroValue:'', loaderTop: true }
     }
 
     componentDidMount() {
        getMovies('vote_average.desc').then(movies => {
            this.setState({
-                popular: movies,
-                loaderPopular: false,
+                top: movies,
+                filtroTop: movies,
+                loaderTop: false,
             })
        })
     }
 
+    handleFilterChange(e){
+        const userValue= e.target.value
+        this.setState({
+          filtroValue: userValue,
+          filtroTop: this.state.top.filter(movie => movie.title.toLowerCase().includes(userValue.toLowerCase()))
+        })
+      }
+
     render() {
         return (
             <div className="home-container">
+                <input type='text' className="input-buscador" placeholder="Filtra película..." onChange= {(e)=>this.handleFilterChange(e)}value={this.state.filtroValue}/>
                 <div className="movies-section">
-                    <ListaPeliculas title={"Top Rated Movies"} loader={this.state.loaderPopular} peliculas={this.state.popular}/> 
+                    <ListaPeliculas title={"Top Rated Movies"} loader={this.state.loaderTop} peliculas={this.state.filtroTop}/> 
                 </div>
                 <div className='load-more-container'>
                     <Button className="dark">
